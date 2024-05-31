dbfb04f8c855e942ed57e9d08814cd268c956f29
jmgv87191 <jmgv87191@gmail.com>
2024-05-30 20:26:51 -0700

    user

--- src/app/components/user-app/user-app.component.ts
+++ src/app/components/user-app/user-app.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from '../../models/user';
 import { UserService } from '../../services/user.service';
+import { UserComponent } from '../user/user.component';
+import { UserFormComponent } from '../user-form/user-form.component';
 
 @Component({
   selector: 'app-user-app',
   standalone: true,
-  imports: [],
+  imports: [ UserComponent,UserFormComponent],
   templateUrl: './user-app.component.html',
   styleUrl: './user-app.component.css'
 })
---
+++ src/app/components/user/user.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core';
+import { User } from '../../models/user';
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css'
+})
+export class UserComponent {
+
+  @Input() users: User[] = []
+
+}
